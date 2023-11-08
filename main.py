620740d6e3f13d8a42228f98968c287608b3aab7
AlexProger777 <you@example.com>
2023-11-08 19:32:36 +0700

    Update

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 import asyncio
 # import datetime
 # import json
-from aiogram.types.reply_keyboard_remove import ReplyKeyboardRemove
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
-from aiogram import exceptions
 #from aiogram.utils.markdown import hbold, hitalic, hlink
 import config
 import keyboards
@@ -29,7 +27,7 @@ async def catalog(message: Message):
 @dp.callback_query(F.data.startswith("cat_"))
 async def brand(callback: CallbackQuery):
     data = callback.data.replace('cat_', '')
-    
+
 
 async def start():
     """Start Telegram Bot"""
